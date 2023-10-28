a6644a808fe55904d15a7a1d6040157bec84d3ba
Illia Pelypenko <pelypenkoillya@gmail.com>
2023-10-28 18:08:44 +0200

    add groups styles

---
+++ normalization.js
@@ -0,0 +1,533 @@
+const serverData = [
+  {
+    "id": "9025b734-8a43-47c6-8700-5721fb66a2c9",
+    "exerciseGroupDto": {
+      "id": 1,
+      "name": "PELVIC MOVEMENTS"
+    },
+    "date": "2023-10-25",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": null,
+    "time08_10": null,
+    "time10_12": 12,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": 4,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "05930d94-0dfb-425d-899c-0df86ae981eb",
+    "exerciseGroupDto": {
+      "id": 1,
+      "name": "PELVIC MOVEMENTS"
+    },
+    "date": "2023-10-26",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 12,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": 2,
+    "time18_20": 60,
+    "time20_22": 60,
+    "time22_24": null
+  },
+  {
+    "id": "7d811aa2-e9fd-4b23-a802-d1e6b537681a",
+    "exerciseGroupDto": {
+      "id": 2,
+      "name": "HIP FLEXORS AND THIGHS FRONT"
+    },
+    "date": "2023-10-26",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": 60,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "46252166-7c04-47e7-ae49-f34436d26257",
+    "exerciseGroupDto": {
+      "id": 3,
+      "name": "LATERAL THIGHS AND BUTTOCKS"
+    },
+    "date": "2023-10-26",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 50,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "5804510c-286b-4f03-8c35-ba133c256f77",
+    "exerciseGroupDto": {
+      "id": 4,
+      "name": "BACK OF THIGHS"
+    },
+    "date": "2023-10-26",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": 80,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "74f7de76-091c-467a-a233-32cc6c2fed0b",
+    "exerciseGroupDto": {
+      "id": 5,
+      "name": "ADDITIONALS"
+    },
+    "date": "2023-10-26",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 30,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": 22,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "fb12b303-6297-4c7c-b4ad-02e6efc37e88",
+    "exerciseGroupDto": {
+      "id": 1,
+      "name": "PELVIC MOVEMENTS"
+    },
+    "date": "2023-10-27",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": 20,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "015c02cf-516a-4354-92a1-59d329b666e3",
+    "exerciseGroupDto": {
+      "id": 3,
+      "name": "LATERAL THIGHS AND BUTTOCKS"
+    },
+    "date": "2023-10-27",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "ef8cd76b-3439-4e7e-bf00-070fa98ed9d9",
+    "exerciseGroupDto": {
+      "id": 4,
+      "name": "BACK OF THIGHS"
+    },
+    "date": "2023-10-27",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": 10,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "9025b736-8a43-47c6-8700-5721fb66a2c9",
+    "exerciseGroupDto": {
+      "id": 1,
+      "name": "PELVIC MOVEMENTS"
+    },
+    "date": "2023-10-24",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": null,
+    "time08_10": null,
+    "time10_12": 12,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": 4,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "05930d64-0dfb-425d-899c-0df86ae981eb",
+    "exerciseGroupDto": {
+      "id": 1,
+      "name": "PELVIC MOVEMENTS"
+    },
+    "date": "2023-10-23",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 12,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": 2,
+    "time18_20": 60,
+    "time20_22": 60,
+    "time22_24": null
+  },
+  {
+    "id": "7d811aa6-e9fd-4b23-a802-d1e6b537681a",
+    "exerciseGroupDto": {
+      "id": 2,
+      "name": "HIP FLEXORS AND THIGHS FRONT"
+    },
+    "date": "2023-10-24",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": 60,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "46252165-7c04-47e7-ae49-f34436d26257",
+    "exerciseGroupDto": {
+      "id": 3,
+      "name": "LATERAL THIGHS AND BUTTOCKS"
+    },
+    "date": "2023-10-24",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 50,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": 11
+  },
+  {
+    "id": "5804515c-286b-4f03-8c35-ba133c256f77",
+    "exerciseGroupDto": {
+      "id": 4,
+      "name": "BACK OF THIGHS"
+    },
+    "date": "2023-10-24",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": 80,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": 33,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "44f7de76-091c-467a-a233-32cc6c2fed0b",
+    "exerciseGroupDto": {
+      "id": 5,
+      "name": "ADDITIONALS"
+    },
+    "date": "2023-10-24",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 30,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "fb32b303-6297-4c7c-b4ad-02e6efc37e88",
+    "exerciseGroupDto": {
+      "id": 1,
+      "name": "PELVIC MOVEMENTS"
+    },
+    "date": "2023-10-22",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": 20,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "014c02cf-516a-4354-92a1-59d329b666e3",
+    "exerciseGroupDto": {
+      "id": 3,
+      "name": "LATERAL THIGHS AND BUTTOCKS"
+    },
+    "date": "2023-10-23",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": 90,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": 90,
+    "time22_24": null
+  },
+  {
+    "id": "ef8cd78b-3639-4e7e-bf00-070fa98ed9d9",
+    "exerciseGroupDto": {
+      "id": 4,
+      "name": "BACK OF THIGHS"
+    },
+    "date": "2023-10-23",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": 10,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "ef8cd76b-3429-4e7e-bf00-070fa98ed9d9",
+    "exerciseGroupDto": {
+      "id": 4,
+      "name": "BACK OF THIGHS"
+    },
+    "date": "2023-10-22",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": 10,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "9025b736-8a23-47c6-8700-5721fb66a2c9",
+    "exerciseGroupDto": {
+      "id": 1,
+      "name": "PELVIC MOVEMENTS"
+    },
+    "date": "2023-10-22",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": null,
+    "time08_10": null,
+    "time10_12": 12,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": 4,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "05930d64-0dfb-225d-899c-0df86ae981eb",
+    "exerciseGroupDto": {
+      "id": 1,
+      "name": "PELVIC MOVEMENTS"
+    },
+    "date": "2023-10-21",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 12,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": 2,
+    "time18_20": 60,
+    "time20_22": 60,
+    "time22_24": null
+  },
+  {
+    "id": "7d811aa6-e9fd-2b23-a802-d1e6b537681a",
+    "exerciseGroupDto": {
+      "id": 2,
+      "name": "HIP FLEXORS AND THIGHS FRONT"
+    },
+    "date": "2023-10-21",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": 60,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": null
+  },
+  {
+    "id": "46252165-7c04-27e7-ae49-f34436d26257",
+    "exerciseGroupDto": {
+      "id": 3,
+      "name": "LATERAL THIGHS AND BUTTOCKS"
+    },
+    "date": "2023-10-21",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 50,
+    "time08_10": null,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": null,
+    "time20_22": null,
+    "time22_24": 11
+  },
+  {
+    "id": "5804515c-286b-2f03-8c35-ba133c256f77",
+    "exerciseGroupDto": {
+      "id": 4,
+      "name": "BACK OF THIGHS"
+    },
+    "date": "2023-10-21",
+    "time00_02": null,
+    "time02_04": null,
+    "time04_06": null,
+    "time06_08": 10,
+    "time08_10": 80,
+    "time10_12": null,
+    "time12_14": null,
+    "time14_16": null,
+    "time16_18": null,
+    "time18_20": 33,
+    "time20_22": null,
+    "time22_24": null
+  }
+];
+
+const dataWithDate = [];
+
+for(const item of serverData) {
+  const date = item.date;
+
+  const dataWithDateItem = dataWithDate.find(i => i.date === date);
+
+  if(!dataWithDateItem) {
+    dataWithDate.push({
+      date,
+      groups: [],
+    });
+  }
+
+  const resultItem = dataWithDate.find(i => i.date === date);
+
+  resultItem.groups.push(
+    {
+      ...item.exerciseGroupDto,
+      periods: Object.entries(item).filter(([key, value]) => key.includes('time')).map(([key, value]) => ({ [key]: value })),
+    }
+  )
+}
+
+function transformData(allData) {
+  let transformed = [];
+
+  allData.forEach(data => {
+    data.groups.forEach(group => {
+      group.periods.forEach(period => {
+        Object.entries(period).forEach(([key, value]) => {
+          if (value !== null) {
+            transformed.push({
+              date: data.date,
+              timeKey: key,
+              value: value,
+              groupId: group.id,
+              groupName: group.name
+            });
+          }
+        });
+      });
+    });
+  })
+
+  return transformed;
+}
+
+
+console.log(JSON.stringify(transformData(dataWithDate), undefined, 2));
--- src/App.js
+++ src/App.js
@@ -1,73 +1,89 @@
 import './App.css';
-import {CartesianGrid, Legend, Scatter, ScatterChart, Tooltip, XAxis, YAxis, ZAxis} from "recharts";
+import {CartesianGrid, Scatter, ScatterChart, Tooltip, XAxis, YAxis } from "recharts";
+import {normalizedData} from "./normalization2";
 
+const groupsStyle = [
+  {
+    backgroundColor: '#50C4E3',
+    borderColor: '#0095CC',
+    id: 2,
+  },
+  {
+    backgroundColor: '#7897FF',
+    borderColor: '#063EF9',
+    id: 3,
+  },
+  {
+    backgroundColor: '#C19CFC',
+    borderColor: '#6308F7',
+    id: 4,
+  },
+  {
+    backgroundColor: '#DCC80F',
+    borderColor: '#A37F00',
+    id: 1,
+  },
+  {
+    backgroundColor: '#FFAC66',
+    borderColor: '#FF7500',
+    id: 5,
+  },
+];
+
+const CHART_HEIGHT = 390;
+const CHART_WIDTH = 965;
 const BAR_WIDTH = 7.5;
-const SPACING = 0;
 
 const TimePeriodShape = (props) => {
   const { cx, cy, payload } = props;
-  const values = Object.values(payload.values);
+
+  const groupId = payload.exerciseGroupId;
+  const value = payload.value;
+
+  const groupStyle = groupsStyle.find(groupStyle => groupStyle.id === groupId)
+  const { backgroundColor, borderColor } = groupStyle;
+
+  const adjustedX = cx + (BAR_WIDTH * groupId);
 
   return (
-    <g transform={`translate(${cx - ((BAR_WIDTH + SPACING) * values.length) / 2}, ${cy})`}>
-      {values.map((value, index) => (
-        <rect
-          key={index}
-          x={(BAR_WIDTH + SPACING) * index}
-          y={-value} // Start drawing from the top to the bottom
-          width={BAR_WIDTH}
-          height={value}
-          rx="1.75"
-          fill="#7897FF"
-          stroke="#063EF9"
-          strokeWidth="0.5"
-        />
-      ))}
-    </g>
+    <rect
+      x={adjustedX}
+      y={cy - (CHART_HEIGHT / 12 / 120) * value - 1}
+      width={BAR_WIDTH}
+      height={(CHART_HEIGHT / 12 / 120) * value}
+      fill={backgroundColor}
+      stroke={borderColor}
+      strokeWidth="0.5"
+      rx="1.75"
+    />
   );
 };
 
-const timePeriods = [
-    { timePeriod: '12 am', timeKey: 'time00_02', date: "2023-10-16" },
-    { timePeriod: '2 am', timeKey: 'time02_04', date: "2023-10-17" },
-    { timePeriod: '4 am', timeKey: 'time04_06', date: "2023-10-18" },
-    { timePeriod: '6 am', timeKey: 'time06_08', date: "2023-10-19" },
-    { timePeriod: '8 am', timeKey: 'time08_10', date: "2023-10-20" },
-    { timePeriod: '10 am', timeKey: 'time10_12', date: "2023-10-21" },
-    { timePeriod: '12 pm', timeKey: 'time12_14', date: "2023-10-22" },
-    { timePeriod: '2 pm', timeKey: 'time14_16', date: "2023-10-23" },
-    { timePeriod: '4 pm', timeKey: 'time16_18', date: "2023-10-24" },
-    { timePeriod: '6 pm', timeKey: 'time18_20', date: "2023-10-25" },
-    { timePeriod: '8 pm', timeKey: 'time20_22', date: "2023-10-26" },
-    { timePeriod: '10 pm', timeKey: 'time22_24', date: "2023-10-27" },
-  ].map(entry => ({
-  ...entry,
-  values: {
-    value1: Math.floor(Math.random() * 101),
-    value2: Math.floor(Math.random() * 101),
-    value3: Math.floor(Math.random() * 101),
-    value4: Math.floor(Math.random() * 101),
-    value5: Math.floor(Math.random() * 101),
-  }
-}));
+const LabelX = ({ x, y, payload }) =>
+  <text x={x} y={y} dy={20} dx={30} className="label" textAnchor="middle">
+    {payload.value}
+  </text>
+
+const LabelY = ({ x, y, payload }) =>
+  <text x={x} y={y} dy={4} dx={-20} className="label labelY" textAnchor="middle">
+    {payload.value}
+  </text>
 
 function App() {
   return (
     <div className="App">
       <ScatterChart
-        width={1031}
-        height={336}
+        width={CHART_WIDTH}
+        height={CHART_HEIGHT}
       >
         <Tooltip />
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" type="category" />
-        <YAxis dataKey="timePeriod" type="category" domain={['10 pm', '12 am']} />
-        <Scatter data={timePeriods} shape={<TimePeriodShape />} />
+        <CartesianGrid vertical={false} stroke="#D9DBE9" />
+        <XAxis dataKey="date" type="category" allowDuplicatedCategory={false} scale="point" interval={0} tick={<LabelX />} padding={{ right: 60}} tickLine={false} axisLine={{ stroke: '#D9DBE9' }}/>
+        <YAxis dataKey="timeKey" type="category" allowDuplicatedCategory={false} scale="point" tick={<LabelY />} padding={{ top: 28}} tickLine={false} axisLine={false}/>
+        <Scatter data={normalizedData} shape={<TimePeriodShape />} />
       </ScatterChart>
     </div>
   );
 }
 
 export default App;
-
-// shape={({ value }) => <TimePeriodShape value={value}/>}
